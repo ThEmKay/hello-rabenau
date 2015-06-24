7d48e70747bed30566438b433413fb3d5d3c257e
FelixEbert <mister_y@web.de>
2015-06-24 17:11:38 +0200

    Class MainContent
    Add class MainContent

---
+++ app/src/main/java/vb/refugeehelpvb/MainContent.java
@@ -0,0 +1,24 @@
+package vb.refugeehelpvb;
+
+public class MainContent {
+
+    private String name;
+    private int thumbnail;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
+
